72aca325b02a3f210b4ebcb9fa137d05f0b08ae5
deanclark1 <ntumydoreen@gmail.com>
2021-06-15 11:28:38 +0000

    footer base

---
+++ wp-content/themes/readyHubb-theme/footer.php
@@ -0,0 +1,11 @@
+<footer>
+   <!-- footer goes here -->
+</footer>  
+
+   
+
+       <!-- scripts refs goes here -->
+    <script src="<?php echo get_theme_file_uri('/js/main.js'); ?>"> </script>
+  
+
+</body>
